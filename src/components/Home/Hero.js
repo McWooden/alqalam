4ac3fbea047b52b7cd9d5a2cc4b0077ac86f92e1
McWooden <garryf7887@gmail.com>
2024-01-28 17:42:58 +0700

    init yang kedua

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 import heroimg from '../../assets/herobg.webp'
 import PrayerTimes from "./PrayerTimes";
 
