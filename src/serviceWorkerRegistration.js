d9b3fcdd6a77396d0a6598aade6b7f38e0fb337d
McWooden <garryf7887@gmail.com>
2024-01-28 17:44:21 +0700

    update service worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -71,6 +71,7 @@ function registerValidSW(swUrl, config) {
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.'
               );
+              alert('Halaman di update, silahkan tutup tab dan buka kembali!')
 
               // Execute callback
               if (config && config.onUpdate) {
